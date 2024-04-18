ac3d784f47ab56be92c8ab07ad478ed1596c490b
BeanbagTheCat <63575685+BeanbagTheCat@users.noreply.github.com>
2024-04-17 23:12:30 -0400

    Update LibrarySystemTestHarness.cpp
    - Added navigate list

--- LibrarySystemTestHarness.cpp
+++ LibrarySystemTestHarness.cpp
@@ -1,12 +1,27 @@
 #include "Book.h"
 #include "reservationQueue.h"
 #include "HashTable.h"
+#include "DoublyLinkedList.h"
+
+#define MENU_ADD_BOOK	 1
+#define MENU_NAVIGATE	 2
+#define NAV_FORWARD		'w'
+#define NAV_BACKWARD	'q'
+#define NAV_EXIT		'x'
+
+typedef struct NavigateList {
+	struct BookList* head;
+	struct BookList* tail;
+} NavigateList;
+
 
 void initializeLibrary(HashTable<char*, Book>* bookTable);
 void displayMenu();
+void createNavigateList(NavigateList** list, HashTable<char*, Book>* bookTable);
 
 int main(void) {
-	HashTable<char*, Book>* bookTable = new HashTable<char*, Book>;
+	HashTable<char*, Book>* bookTable = new HashTable<char*, Book>();
+	HashTable<char*, queue>* queueTable = new HashTable<char*, queue>();
 	initializeLibrary(bookTable);
 	struct BookList* head = NULL;
 	struct Booklist* tail = NULL;
@@ -16,39 +31,89 @@ int main(void) {
 	while (run) {
 		displayMenu();
 		printf("Enter your choice: ");
-		scanf("%d", choice);
-
+		scanf("%d%*c", &choice);
+		printf("%d CHOICE\n", choice);
 		switch (choice) {
-		case 1:
-			int bookDay, bookMonth, bookYear, bookAvailablity, bookReviews = 0;
+		case MENU_ADD_BOOK: {
+			int bookDay, bookMonth, bookYear, bookAvailability, bookReviews = 0;
 			float bookRatings = 0;
-			char bookName[50], bookAuthor[50], bookGenre[50] = {};
+			char bookName[50], bookAuthor[50], bookGenre[50], bookDayBuff[3], bookMonthBuff[3], bookYearBuff[5], bookAvailBuff[2], bookReviewBuff[10], bookRatingsBuff[3] = {};
 			printf("\nEnter book title: ");
-			scanf("%49s", bookName);
+			fgets(bookName, 50, stdin);
 
 			printf("\nEnter book author: ");
-			scanf("%49s", bookAuthor);
+			fgets(bookAuthor, 50, stdin);
 
 			printf("\nEnter book Genre: ");
-			scanf("%49s", bookGenre);
+			fgets(bookGenre, 50, stdin);
 
 			printf("\nEnter book publication date: ");
-			scanf("%49d", bookDay);
-			scanf("%49d", bookMonth);
-			scanf("%49d", bookYear);
+			bookDay = atoi(fgets(bookDayBuff, 3, stdin));
+			bookMonth = atoi(fgets(bookMonthBuff, 3, stdin));
+			bookYear = atoi(fgets(bookYearBuff, 5, stdin));
 
 			printf("\nEnter book Availablity: ");
-			scanf("%49d", bookAvailablity);
+			bookAvailability = atoi(fgets(bookAvailBuff, 2, stdin));
 
 			printf("\nEnter book Reviews: ");
-			scanf("%49d", bookReviews);
+			bookReviews = atoi(fgets(bookReviewBuff, 10, stdin));
 
 			printf("\nEnter book Ratings: ");
-			scanf("%1.1f", bookRatings);
+			bookRatings = atof(fgets(bookRatingsBuff, 3, stdin));
 
-			Book* newBook = new Book(bookName, bookName, bookAuthor, bookGenre, bookDay, bookMonth, bookYear, bookAvailablity, bookReviews, bookRatings);
+			Book* newBook = new Book(bookName, bookName, bookAuthor, bookGenre, bookDay, bookMonth, bookYear, bookAvailability, bookReviews, bookRatings);
 			bookTable->AddElement(newBook->GetId(), newBook);
 			break;
+		}
+		case MENU_NAVIGATE: {
+			bool navMode = true;
+			NavigateList* navList = (NavigateList*)malloc(sizeof NavigateList);
+			if (navList == NULL) {
+				exit(EXIT_FAILURE);
+			}
+			createNavigateList(&navList, bookTable);
+			BookList* current = navList->head;
+			char navChoice;
+			while (navMode) {
+				printf("Navigating books\n");
+				PrintBook(current);
+				printf(
+					"\n\nw -> Move Forward\n"
+					"q -> Move Backward\n"
+					"x -> Exit to main menu\n"
+				);
+				scanf("%c%*c", &navChoice);
+
+
+				switch (navChoice) {
+				case(NAV_FORWARD): {
+					if (current->NextBook != NULL) {
+						current = current->NextBook;
+					}
+					else {
+						printf("At end of list\n");
+					}
+					break;
+				}
+				case(NAV_BACKWARD): {
+					if (current->PreviousBook != NULL) {
+						current = current->PreviousBook;
+					}
+					else {
+						printf("At front of list\n");
+					}
+					break;
+				}
+				case(NAV_EXIT): {  
+					navMode = false;
+				}
+
+				}
+			};
+
+
+		}
+
 
 		default:
 
@@ -61,6 +126,18 @@ int main(void) {
 	}
 
 
+}
+
+
+void createNavigateList(NavigateList** list, HashTable<char*, Book>* bookTable) {
+	Book** values = bookTable->GetValues();
+	BookList* head = NULL;
+	BookList* tail = NULL;
+	for (int i = 0; i < bookTable->GetSize();i++) {
+		AddNewBook(&head, &tail, values[i]);
+	} 
+	(*list)->head = head;
+	(*list)->tail = tail;
 }
 
 
