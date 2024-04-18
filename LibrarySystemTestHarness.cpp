40aaf3b3d6fa3834f42cd52bb5a313b02e0b7285
BethChiray <157905488+BethChiray@users.noreply.github.com>
2024-04-17 20:31:38 -0400

    Update LibrarySystemTestHarness.cpp

--- LibrarySystemTestHarness.cpp
+++ LibrarySystemTestHarness.cpp
@@ -3,12 +3,14 @@
 #include "HashTable.h"
 
 void initializeLibrary(HashTable<char*, Book>* bookTable);
-
+void displayMenu();
 
 int main(void) {
 	HashTable<char*, Book>* bookTable = new HashTable<char*, Book>;
 	queue* holdQueue = initializeQueue();
 	initializeLibrary(bookTable);
+	struct BookList* head = NULL;
+	struct Booklist* tail = NULL;
 
 	printf("%s", (*bookTable->GetElementByKey((char*)"h")).GetAuthor());
 	printf("%s", (*bookTable->GetElementByKey((char*)"book2")).GetAuthor());
@@ -18,6 +20,14 @@ int main(void) {
 	printf("%s", (*bookTable->GetElementByKey((char*)"h")).GetAuthor());
 	//printf("%s", (*bookTable->GetElementByKey((char*)"book2")).GetAuthor());
 	bookTable->GetElementByKey((char*)"book2");
+
+	printf("Welcome to ShelfSensei the Library Management System");
+
+	while(true) {
+		
+	}
+
+	
 }
 
 
@@ -26,5 +36,13 @@ void initializeLibrary(HashTable<char*, Book>* bookTable) {
 	Book* b2 = new Book((char*)"book2", (char*) "Clean Code", (char*) "Robert Cecil Martin", (char*) "Non-Fiction", 1, 8, 2008, 1);
 	bookTable->AddElement(b1->GetId(), b1);
 	bookTable->AddElement(b2->GetId(), b2);
+}
+
+void displayMenu() {
+	printf("\nMenu:\n");
+	printf("1. Add Book to System\n");
+	printf("2. View Books in System\n");
+	printf("3. Manage Books in Holding Queue\n");
+	printf("4. Exit\n");
 }
 
